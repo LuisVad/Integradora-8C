b76396bb8654b840c99b117f1ded49193f73e6a0
Luis Valladolid <20213tn002@utez.edu.mx>
2024-02-10 22:16:15 -0600

    Subida de BackEnd y MOVIDA DE FRONT EN UNA CARPETA INDEPENDIENTE POR QUE SE LES OLVIDO FOSTER

---
+++ BackEnd/Integradora8C/src/test/java/utez/edu/mx/Integradora8C/Integradora8CApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.Integradora8C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integradora8CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
