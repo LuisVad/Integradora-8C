43a81674ff392298271713f3d8fa35d87fbf3a7d
Ivan Samuel <20213tn073@utez.edu.mx>
2024-03-13 18:42:59 -0600

    [ADD] Twilio Services

---
+++ BackEnd/Integradora8C/src/main/java/utez/edu/mx/Integradora8C/Services/Twilio/TwilioServices.java
@@ -0,0 +1,35 @@
+package utez.edu.mx.Integradora8C.Services.Twilio;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import utez.edu.mx.Integradora8C.utils.Response;
+
+@Service
+@Transactional
+public class TwilioServices {
+    @Value("${TWILIO_ACCOUNT_SID}")
+    String sid;
+    @Value("${TWILIO_AUTH_TOKEN}")
+    String token;
+    @Value("${TWILIO_NUMBER}")
+    String phoneNumber;
+
+    public Response<String> sendSMS(String number){
+        Twilio.init(sid, token);
+        Message.creator(
+                new PhoneNumber(number),
+                new PhoneNumber(phoneNumber),
+                "Probando"
+        ).create();
+        return new Response<>(
+                "Enviado",
+                false,
+                200,
+                "OK"
+        );
+    }
+}
